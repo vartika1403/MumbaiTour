3f042f70e205a9fdeb9f016ff8465da90936fc28
vartika1403 <vartika.1403@gmail.com>
2017-04-18 20:13:28 +0530

    added grid layout and items

--- app/src/main/java/com/example/vartikasharma/mumbaitour/ListGridActivity.java
+++ app/src/main/java/com/example/vartikasharma/mumbaitour/ListGridActivity.java
@@ -14,37 +14,44 @@ import butterknife.ButterKnife;
 
 public class ListGridActivity extends AppCompatActivity {
 
-    @BindView(R.id.vertical_linear_layout)
-    public LinearLayout VerticalLinearLayout;
+    private String[] itemList = {"Weather", "Pubs and Bars", "Hotels", "Train Route", "Must Visit", "Feedback"};
 
+    /*@BindView(R.id.vertical_linear_layout)
+    public LinearLayout VerticalLinearLayout;
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_grid);
         ButterKnife.bind(this);
 
-        VerticalLinearLayout.removeAllViews();
-        for(int i = 0; i < 3; i++) {
-          LinearLayout linearHorizontal  =  addItemsToLayout();
+       /* VerticalLinearLayout.removeAllViews();
+        for(int i = 0; i < 2; i++) {
+          LinearLayout linearHorizontal  =  addItemsToLayout(i);
             VerticalLinearLayout.addView(linearHorizontal);
-        }
+        }*/
     }
 
-    private LinearLayout addItemsToLayout() {
+    private LinearLayout addItemsToLayout(int i) {
         View view = LayoutInflater.from(this).inflate(R.layout.item_layout, null);
         LinearLayout horizontalLinearLayout = (LinearLayout)view.findViewById(R.id.horizontal_linerar_layout);
 
         horizontalLinearLayout.removeAllViews();
-        for(int i = 0; i < 3; i++) {
-            Button itemText = new Button(this);
+       int x;
+        if (i == 0 ) {
+            x = 0;
+        } else {
+            x = 3;
+        }
+        for(int j = x; j < x + 2; i++) {
+            TextView itemText = new TextView(this);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             params.setMargins(40, 40, 40, 40);
             itemText.setLayoutParams(params);
-         //   itemText.setLeft(30);
-            itemText.setText("Weather");
+            itemText.setText(itemList[j]);
             itemText.setTextSize(15);
-            itemText.setTextColor(Color.parseColor("#0B5345"));
+          //  itemText.setTextColor(Color.parseColor("#0B5345"));
             horizontalLinearLayout.addView(itemText);
         }
         return horizontalLinearLayout;
