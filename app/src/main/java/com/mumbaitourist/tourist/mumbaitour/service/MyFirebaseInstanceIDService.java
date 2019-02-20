35defc58d70bede162bd57242d191c8c4d9d03ee
vartika1403 <vartika.1403@gmail.com>
2019-02-21 01:17:57 +0530

    added gradle changes

---
+++ app/src/main/java/com/mumbaitourist/tourist/mumbaitour/service/MyFirebaseInstanceIDService.java
@@ -0,0 +1,6 @@
+package com.mumbaitourist.tourist.mumbaitour.service;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+}
