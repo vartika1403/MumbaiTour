27a67de7a2660cb53fcded4c0812cb6e85a2efb9
vartika1403 <vartika.1403@gmail.com>
2019-03-03 19:25:14 +0530

    changed mumbai cover page

--- app/src/main/java/com/mumbaitourist/tourist/mumbaitour/HomeActivity.java
+++ app/src/main/java/com/mumbaitourist/tourist/mumbaitour/HomeActivity.java
@@ -59,7 +59,6 @@ public class HomeActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
             if (count == 1) {
                 Log.i(LOG_TAG, "onBack press count 0");
                 finish();
